e0b8de470b10b4394cf911e02233d8d364d7c839
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-04-01 14:46:51 -0400

    lab went over in class

--- server/app.py
+++ server/app.py
@@ -4,5 +4,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  #alt way of starting py
     app.run(port=5555, debug=True)
+
+@app.route("/") #setting up my route
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>', 200 #returning a status code
+
+
+@app.route('/print/<string:parameter>') # you need to pass the datatype in the parameter 
+def print_string(parameter):
+    print(parameter)  # Printing the parameter in the console
+    return parameter, 200
+# <parameter>: This part of the route acts as a placeholder for any value that comes after /print/. It tells Flask to capture whatever value is present at this position in the URL and pass it as an argument to the corresponding view function.
+
+@app.route('/count/<int:parameter>') 
+def count(parameter):
+    result = ''
+    for num in range(parameter):
+        result += str(num) + '\n'
+        
+    return result, 200
+
+
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return  str(num1 + num2), 200
+    elif operation == '-':
+        return  str(num1 - num2), 200   
+    elif operation == '*':
+        return  str(num1 * num2), 200
+    elif operation == 'div':
+        return  str(num1 / num2), 200
+    elif operation == '%':
+        return  str(num1 % num2), 200
+    else:
+        return f'invalid operator: [operation]', 404
+    
